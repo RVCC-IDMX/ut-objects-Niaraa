ac0c7d969eff27ba52dea4005cf007d9762f155c
Niara <123399425+Niaraa@users.noreply.github.com>
2023-11-25 20:25:47 -0500

    passed all

--- src/resistor.js
+++ src/resistor.js
@@ -48,6 +48,18 @@
      silver: 0.01
   };
  */
+const colorCodes = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
 
 /**
  * Returns the digit as a number from the resistor color code
@@ -61,9 +73,22 @@
  * and put it inside this function so it is private
  * then use the copied object like a lookup table
  */
+const multiplierCodes = {
+  black: 1,
+  brown: 10,
+  red: 100,
+  orange: 1000,
+  yellow: 10000,
+  green: 100000,
+  blue: 1000000,
+  violet: 10000000,
+  grey: 100000000,
+  white: 1000000000,
+  gold: 0.1,
+  silver: 0.01,
+};
 function getColorValue(color) {
-  // eslint-disable-next-line no-undef
-  return colorValue[color];
+  return colorCodes[color];
 }
 
 /**
@@ -80,7 +105,6 @@ function getColorValue(color) {
  * then use the copied object like a lookup table
  */
 function getMultiplierValue(color) {
-  // eslint-disable-next-line no-undef
   return multiplierCodes[color];
 }
 
@@ -145,12 +169,10 @@ function formatNumber(val) {
   if (value >= 1000000000) {
     value /= 1000000000;
     metric = 'G';
-  }
-  if (value >= 1000000) {
+  } else if (value >= 1000000) {
     value /= 1000000;
     metric = 'M';
-  }
-  if (value >= 1000) {
+  } else if (value >= 1000) {
     value /= 1000;
     metric = 'k';
   }
